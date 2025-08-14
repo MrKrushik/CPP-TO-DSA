106ff8a23e77c8072a38b1b3aef8afa9214c8b4c
Krushik Rakholiya <itzmekrushik90999@gmail.com>
2025-08-14 11:12:44 +0530

    VARIABLE,DATATYPE,CONSTANT

---
+++ CH-2 VARIABLE,DATATYPE,CONSTANT/2.1/QUESTION/Q1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int int main()
+{
+    int a=30, b=10;
+
+    cout <<"addition of 30 and 10 is 40"<<endl;
+    cout <<"subtraction of 30 and 10 is 20"<<endl;
+    cout <<"multiplication of 30 and 10 is 300"<<endl;
+    cout <<"division of 30 and 10 is 3"<<endl;
+    cout <<"modulus of 30 and 10 is 0"<<endl<<endl;
+
+    cout << a << " + " << b << " = " << a + b << endl;
+    cout << a << " - " << b << " = " << a - b << endl;
+    cout << a << " * " << b << " = " << a * b << endl;
+    cout << a << " / " << b << " = " << a / b << endl;
+    cout << a << " % " << b << " = " << a % b << endl;
+}
---
+++ CH-2 VARIABLE,DATATYPE,CONSTANT/2.1/variable.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"daatype, constant , variable"<<endl
+    <<endl;
+
+    // syntax : datatype variable_name ;
+
+    int num = 12;
+    float PI = 3.14;
+    char name = 'R';
+
+    cout << "Number\t: " << num << "." << endl;
+    cout << "PI\t: " << PI << endl;
+    cout << "Name\t: " << name << endl;
+}
